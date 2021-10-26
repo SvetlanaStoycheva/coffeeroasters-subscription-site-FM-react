8c8bc8130253bf4152310dccd84c992cf235f415
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-10-26 15:41:03 +0300

    modal completed

--- src/components/planMain.js
+++ src/components/planMain.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { planSteps, planQuestions } from '../utils/constants';
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+import { FaTimes } from 'react-icons/fa';
 
 const PlanMain = () => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -10,6 +11,9 @@ const PlanMain = () => {
   const [property_03, setProperty_03] = useState('');
   const [property_04, setProperty_04] = useState('');
   const [property_05, setProperty_05] = useState('');
+  const [activeModal, setActiveModal] = useState(false);
+  const [price, setPrice] = useState('');
+  const [closeModal, setCloseModal] = useState(false);
 
   const toggleActiveQuestion = (index) => {
     if (activeIndex === index) {
@@ -20,8 +24,15 @@ const PlanMain = () => {
   const handleChoosenAnswer = (index) => {
     setActiveAnswerIndex(index);
 
-    let name = planQuestions.find((item) => item.id - 1 === activeIndex);
-    name = name.answers.find((answ) => answ.id - 1 === index).title;
+    const properties = planQuestions.find(
+      (item) => item.id - 1 === activeIndex
+    );
+    const name = properties.answers.find((answ) => answ.id - 1 === index).title;
+    let price = properties.answers.find((answ) => answ.id - 1 === index).text;
+    // finding the price to put it in the moduce
+    const indexForPrice = price.indexOf('p');
+    price = price.slice(0, indexForPrice);
+
     // console.log(name);
     if (activeIndex === 0) {
       setProperty_01(name);
@@ -33,11 +44,12 @@ const PlanMain = () => {
       setProperty_04(name);
     } else if (activeIndex === 4) {
       setProperty_05(name);
+      setPrice(price);
     }
   };
 
-  const handleCreatePlan = () => {
-    console.log('click');
+  const handleActiveModal = () => {
+    setActiveModal(true);
   };
 
   return (
@@ -112,7 +124,7 @@ const PlanMain = () => {
         </article>
         <div className='plan-btn-container'>
           <button
-            onClick={handleCreatePlan}
+            onClick={handleActiveModal}
             disabled={
               !property_01 ||
               !property_02 ||
@@ -134,6 +146,45 @@ const PlanMain = () => {
           </button>
         </div>
       </div>
+      {/* modal */}
+      {/* {activeModal && ( */}
+      <article
+        className={closeModal ? 'modal-overlay' : 'show-modal modal-overlay'}
+      >
+        <div className='summary-container modal-container'>
+          <div className='modal-header'>
+            <h3>order summary</h3>
+            <button
+              className='close-modal-btn'
+              type='button'
+              onClick={() => {
+                setCloseModal(true);
+              }}
+            >
+              <FaTimes />
+            </button>
+          </div>
+
+          <h4>
+            “I drink my coffee using{'  '}
+            <span> {property_01 ? property_01 : '_____'}</span>, with a{' '}
+            <span> {property_02 ? property_02 : '_____'}</span> type of bean.{' '}
+            <span> {property_03 ? property_03 : '_____'}</span> ground ala
+            <span> {property_04 ? property_04 : '_____'}</span>, sent to me{' '}
+            <span> {property_05 ? property_05 : '_____'}</span>.”
+          </h4>
+          <p>
+            Is this correct? You can proceed to checkout or go back to plan
+            selection if something is off. Subscription discount codes can also
+            be redeemed at the checkout.
+          </p>
+          <div className='modal-footer'>
+            <h4>{`${price} / shipment`}</h4>
+            <div className='checkout-btn btn'>Checkout</div>
+          </div>
+        </div>
+      </article>
+      {/* )} */}
     </section>
   );
 };
